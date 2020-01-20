04c82f9b30a0dd6f22e4da5a776d4d76fe6e4de0
ahmedkeskin <ahmedkeskin@gmail.com>
2020-01-20 16:54:15 +0300

    Hotfix

--- Form1.cs
+++ Form1.cs
@@ -13,19 +13,21 @@ namespace DgsDayCounter
     public partial class Form1 : Form
     {
         DateTime _goalDate = new DateTime(2020, 7, 5,10,15,00);
+        System.Globalization.CultureInfo _cultureinfo;
         public Form1()
         {
             InitializeComponent();
-            lblDate.Text = _goalDate.ToString();
+            lblDate.Text = _goalDate.ToString("MM/dd/yyyy HH:mm:ss", _cultureinfo);
+            _cultureinfo = new System.Globalization.CultureInfo("tr-TR");
         }
 
         private void tmr_Tick(object sender, EventArgs e)
         {
             var now = DateTime.Now;
-            var day= _goalDate.Subtract(now).Days.ToString("##");
-            var hour = _goalDate.Subtract(now).Hours.ToString("##");
-            var minute = _goalDate.Subtract(now).Minutes.ToString("##");
-            var second = _goalDate.Subtract(now).Seconds.ToString("##");
+            var day= _goalDate.Subtract(now).Days.ToString("##", _cultureinfo);
+            var hour = _goalDate.Subtract(now).Hours.ToString("##", _cultureinfo);
+            var minute = _goalDate.Subtract(now).Minutes.ToString("##", _cultureinfo);
+            var second = _goalDate.Subtract(now).Seconds.ToString("##", _cultureinfo);
             lblHour.Text = $"Last:{day} days, {hour}:{minute}:{second}";
 
 
