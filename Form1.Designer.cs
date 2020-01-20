d821b0403a6fd2bd7f17afd0b45e363420325db8
ahmedkeskin <ahmedkeskin@gmail.com>
2020-01-20 16:32:56 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace DgsDayCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblRemain = new System.Windows.Forms.Label();
+            this.lblHour = new System.Windows.Forms.Label();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblDate
+            // 
+            this.lblDate.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(52, 0);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(169, 23);
+            this.lblDate.TabIndex = 0;
+            this.lblDate.Text = "05/07/2020";
+            this.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblRemain
+            // 
+            this.lblRemain.BackColor = System.Drawing.Color.Transparent;
+            this.lblRemain.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRemain.Location = new System.Drawing.Point(4, 18);
+            this.lblRemain.Name = "lblRemain";
+            this.lblRemain.Size = new System.Drawing.Size(121, 14);
+            this.lblRemain.TabIndex = 1;
+            this.lblRemain.Text = "Remain Day: 0";
+            this.lblRemain.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblHour
+            // 
+            this.lblHour.BackColor = System.Drawing.Color.Transparent;
+            this.lblHour.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHour.Location = new System.Drawing.Point(131, 18);
+            this.lblHour.Name = "lblHour";
+            this.lblHour.Size = new System.Drawing.Size(161, 14);
+            this.lblHour.TabIndex = 2;
+            this.lblHour.Text = "Remain HOUR: 0";
+            this.lblHour.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tmr
+            // 
+            this.tmr.Enabled = true;
+            this.tmr.Interval = 1000;
+            this.tmr.Tick += new System.EventHandler(this.tmr_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 42);
+            this.Controls.Add(this.lblHour);
+            this.Controls.Add(this.lblRemain);
+            this.Controls.Add(this.lblDate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DGS TIME TRACKER";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblRemain;
+        private System.Windows.Forms.Label lblHour;
+        private System.Windows.Forms.Timer tmr;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DgsDayCounter
+{
+    public partial class Form1 : Form
+    {
+        DateTime _goalDate = new DateTime(2020, 7, 5);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tmr_Tick(object sender, EventArgs e)
+        {
+            lblRemain.Text = $"Remain Day: {DateTime.Now.Subtract(_goalDate).ToString("dd")}";
+            lblHour.Text = $"Remain Hour: {_goalDate.Subtract(DateTime.Now).TotalHours.ToString("#.##")}";
+        }
+    }
+}
