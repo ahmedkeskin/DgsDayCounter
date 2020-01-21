c1ef3cdcb44ce50872b508926aeccc89286b39cb
ahmedkeskin <ahmedkeskin@gmail.com>
2020-01-21 08:17:30 +0300

    Leading zero In DateTime

--- Form1.cs
+++ Form1.cs
@@ -24,10 +24,10 @@ namespace DgsDayCounter
         private void tmr_Tick(object sender, EventArgs e)
         {
             var now = DateTime.Now;
-            var day= _goalDate.Subtract(now).Days.ToString("##", _cultureinfo);
-            var hour = _goalDate.Subtract(now).Hours.ToString("##", _cultureinfo);
-            var minute = _goalDate.Subtract(now).Minutes.ToString("##", _cultureinfo);
-            var second = _goalDate.Subtract(now).Seconds.ToString("##", _cultureinfo);
+            var day= _goalDate.Subtract(now).Days.ToString();
+            var hour = _goalDate.Subtract(now).Hours.ToString("00");
+            var minute = _goalDate.Subtract(now).Minutes.ToString("00");
+            var second = _goalDate.Subtract(now).Seconds.ToString("00");
             lblHour.Text = $"Last:{day} days, {hour}:{minute}:{second}";
 
 
